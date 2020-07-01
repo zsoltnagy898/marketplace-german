e52b4b152d1eb0ab340be187e1dfea9f9cb026ef
Zsolt Nagy <zsolt.nagy@vodafone.com>
2020-07-01 13:19:50 +0200

    created test theme, added lists on homepage, added register icon, padding to icons

--- vodafone-global-storefront-theme/config/index.js
+++ vodafone-global-storefront-theme/config/index.js
@@ -4,8 +4,6 @@ const config = {
 	env: "dev",
 	theme: "vodafone",
 	marketplacePath: "https://testvfgermany.appdirect.com",
-	// marketplacePath: "https://cloud-apps.vodafone.com",
-	// pageObjectsPath: "https://marketplace.appdirect.com/api/internal/storefront/v1",
 	pageObjectsPath: "https://testvfgermany.appdirect.com/api/internal/storefront/v1",
 	publicCSSGenPath: "/assets/css/pages",
 	publicJSPath: "/assets/css/images",
